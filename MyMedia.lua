98146a0b825963a6d4db7feb80b6f782609c1809
nistaux <nistaux@gmail.com>
2026-01-27 15:14:35 -0600

    init

---
+++ MyMedia.lua
@@ -0,0 +1,57 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "DropShadow", [[Interface\Addons\SharedMedia_MyMedia\border\Border_DropShadow.blp]])
+-- -----
+--   FONT
+-- -----
+-- ElvUI Fonts
+LSM:Register("font", "Steelfish", [[Interface\Addons\SharedMedia_MyMedia\font\Steelfish.ttf]])
+LSM:Register("font", "ActionMan", [[Interface\Addons\SharedMedia_MyMedia\font\ActionMan.ttf]])
+LSM:Register("font", "ContinuumMedium", [[Interface\Addons\SharedMedia_MyMedia\font\ContinuumMedium.ttf]])
+LSM:Register("font", "DieDieDie", [[Interface\Addons\SharedMedia_MyMedia\font\DieDieDie.ttf]])
+LSM:Register("font", "Expressway", [[Interface\Addons\SharedMedia_MyMedia\font\Expressway.ttf]])
+LSM:Register("font", "Homespun", [[Interface\Addons\SharedMedia_MyMedia\font\Homespun.ttf]])
+LSM:Register("font", "Invisible", [[Interface\Addons\SharedMedia_MyMedia\font\Invisible.ttf]])
+LSM:Register("font", "PTSansNarrow", [[Interface\Addons\SharedMedia_MyMedia\font\PTSansNarrow.ttf]])
+
+-- ToxiUI Fonts 
+LSM:Register("font", "- ToxiUI Icons", [[Interface\Addons\SharedMedia_MyMedia\font\ToxiUIIcons.ttf]])
+LSM:Register("font", "- ToxiUI", [[Interface\Addons\SharedMedia_MyMedia\font\ToxiUI.ttf]])
+LSM:Register("font", "- Big Noodle Titling", [[Interface\Addons\SharedMedia_MyMedia\font\BigNoodleToo.ttf]])
+LSM:Register("font", "- M 500", [[Interface\Addons\SharedMedia_MyMedia\font\Montserrat-Medium.ttf]])
+LSM:Register("font", "- M 700", [[Interface\Addons\SharedMedia_MyMedia\font\Montserrat-Bold.ttf]])
+LSM:Register("font", "- M 900", [[Interface\Addons\SharedMedia_MyMedia\font\Montserrat-Black.ttf]])
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+-- ToxiUI General Textures
+LSM:Register("statusbar", "BuiOnePixel", [[Interface\Addons\SharedMedia_MyMedia\statusbar\BuiOnePixel.tga]])
+LSM:Register("statusbar", "- ToxiUI", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-clean.tga]])
+LSM:Register("statusbar", "- Tx Fade", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-fade.tga]])
+-- ToxiUI Gradient Textures
+LSM:Register("statusbar", "- Tx Mid", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-grad.tga]])
+LSM:Register("statusbar", "- Tx Left", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-g1.tga]])
+LSM:Register("statusbar", " Tx Right", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-g2.tga]])
+-- d1 Texture for Shadowed Borders
+LSM:Register("statusbar", " d1 shadow", [[Interface\Addons\SharedMedia_MyMedia\statusbar\d1.tga]])
+LSM:Register("statusbar", " d1 shadow border", [[Interface\Addons\SharedMedia_MyMedia\statusbar\d1-border.tga]])
+-- ToxiUI Alternatives (not in use)
+LSM:Register("statusbar", "- Tx Half", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-half.tga]])
+LSM:Register("statusbar", "- Tx Half Fade", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-half-transparent.tga]])
+LSM:Register("statusbar", "- Tx Half Fade Stroke", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-half-transparent-stroke.tga]])
+LSM:Register("statusbar", "- Tx Dark", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ToxiUI-dark.tga]])
+LSM:Register("statusbar", "- Bezo", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Bezo.blp]])
+LSM:Register("statusbar", "- Bezo Dark", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Bezo-dark1.blp]])
+LSM:Register("statusbar", "- Bezo Darker", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Bezo-dark2.blp]])
